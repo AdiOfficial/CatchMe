baf3d65b1332b9d512f5db44ca8eea0fb7ff74ae
Ömer Can Koç <62075091+omercankoc@users.noreply.github.com>
2020-03-19 16:17:24 +0300

    added runnable and handler
    Signed-off-by: Ömer Can Koç <62075091+omercankoc@users.noreply.github.com>

--- app/src/main/java/com/omercankoc/catchme/MainActivity.kt
+++ app/src/main/java/com/omercankoc/catchme/MainActivity.kt
@@ -4,18 +4,59 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.os.Handler
 import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
+
+    //
     var score : Int = 0
+    // ImageView'ları bir dizi icersine aktar.
+    var imageViewArray = ArrayList<ImageView>()
+    //
+    var handler = Handler()
+    //
+    var runnable = Runnable {  }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // ImageView'ların imageViewArray icerisine aktarilmasi
+        imageViewArray.add(imageView1)
+        imageViewArray.add(imageView2)
+        imageViewArray.add(imageView3)
+        imageViewArray.add(imageView4)
+        imageViewArray.add(imageView5)
+        imageViewArray.add(imageView6)
+        imageViewArray.add(imageView7)
+        imageViewArray.add(imageView8)
+        imageViewArray.add(imageView9)
+        imageViewArray.add(imageView10)
+        imageViewArray.add(imageView11)
+        imageViewArray.add(imageView12)
+        imageViewArray.add(imageView13)
+        imageViewArray.add(imageView14)
+        imageViewArray.add(imageView15)
+        imageViewArray.add(imageView16)
+        imageViewArray.add(imageView17)
+        imageViewArray.add(imageView18)
+        imageViewArray.add(imageView19)
+        imageViewArray.add(imageView20)
+        imageViewArray.add(imageView21)
+        imageViewArray.add(imageView22)
+        imageViewArray.add(imageView23)
+        imageViewArray.add(imageView24)
+
+        // Rasgele noktalarin gorunmez olmasi
+        hideImages()
 
         // Geri Sayım
         // 1 dk boyunca her saniye (ms olarak verir)
@@ -29,6 +70,7 @@ class MainActivity : AppCompatActivity() {
                 val alert = AlertDialog.Builder(this@MainActivity)
                 alert.setTitle("Game")
                 alert.setMessage("Restart the game?")
+
                 alert.setPositiveButton("Yes"){dialog, which ->
                     val intent = intent
                     finish() // activity tamamen sonlandirilir.
@@ -45,6 +87,25 @@ class MainActivity : AppCompatActivity() {
                 Time.text = "Time" + millisUntilFinished/1000
             }
         }.start() // baslat.
+    }
+
+    // imageViewArray icerisindeki her image'i gorunmez yap.
+    fun hideImages(){
+        runnable = object : Runnable{
+            override fun run() {
+                for(image in imageViewArray){
+                    image.visibility = View.INVISIBLE
+                }
+
+                // Random sinifi ile olusturulmus
+                val random = Random()
+                val randomIndex = random.nextInt(24)
+                imageViewArray[randomIndex].visibility = View.VISIBLE
+
+                handler.postDelayed(runnable,500)
+            }
+        }
+        handler.post { runnable }
     }
 
     fun IncreaseScore(view : View){
